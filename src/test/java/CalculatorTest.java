7aa41b7368ed7a8ed14459699047f8bfbbc864c9
admintimiyaki <timich.cs@icloud.com>
2025-10-16 20:28:21 +0100

    Add failing test for Calculator.subtract()

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,18 +2,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
 public class CalculatorTest {
     @Test
     void testAdd() {
         Calculator calculator = new Calculator();
-        assertEquals(9, calculator.add(2, 7)); // expected to pass
+        assertEquals(9, calculator.add(2, 7)); // passing test
     }
+
     @ParameterizedTest
     @CsvSource({
             "1, 2, 3"
     })
     void testAddWithAnotherInput(int a, int b, int expected) {
         Calculator calculator = new Calculator();
-        assertEquals(expected, calculator.add(a, b)); // expected to pass
+        assertEquals(expected, calculator.add(a, b)); // passing test
+    }
+
+    @Test
+    void testSubtract() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.subtract(10, 5)); // expected to fail, subtract not implemented yet
     }
 }

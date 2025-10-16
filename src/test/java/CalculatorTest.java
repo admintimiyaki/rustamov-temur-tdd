280c37af869afe6d3966ab0f26b308f34fbe74d3
admintimiyaki <timich.cs@icloud.com>
2025-10-16 20:10:34 +0100

    Add minimal code in Calculator for Calculator.add() to pass the test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,8 +3,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
     @Test
-    void testAdd() { // here it is expected to fail, I still haven't created Calculator.java as for now
+    void testAdd() {
         Calculator calculator = new Calculator();
-        assertEquals(9, calculator.add(2, 7)); // failing test
+        assertEquals(9, calculator.add(2, 7)); // expected to pass
     }
 }

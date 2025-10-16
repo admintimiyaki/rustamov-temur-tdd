4b507be116f936239dd820fa78143a8831942aab
admintimiyaki <timich.cs@icloud.com>
2025-10-16 20:18:17 +0100

    Add parameterized test case 1+2=3 for Calculator.add()

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,10 +1,19 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 public class CalculatorTest {
     @Test
     void testAdd() {
         Calculator calculator = new Calculator();
         assertEquals(9, calculator.add(2, 7)); // expected to pass
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "1, 2, 3"
+    })
+    void testAddWithAnotherInput(int a, int b, int expected) {
+        Calculator calculator = new Calculator();
+        assertEquals(expected, calculator.add(a, b)); // expected to fail
     }
 }

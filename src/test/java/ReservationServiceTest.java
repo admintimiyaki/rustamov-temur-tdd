d7324338bd14ced130be55c3acf324cf581dc5b7
admintimiyaki <timich.cs@icloud.com>
2025-10-17 17:29:24 +0100

    Add failing test for ReservationService.reserve() for the case when no available copies

--- src/test/java/ReservationServiceTest.java
+++ src/test/java/ReservationServiceTest.java
@@ -15,4 +15,14 @@ public class ReservationServiceTest {
         assertTrue(reservationRepository.existsByUserAndBook("u1", "bk1"));
         assertEquals(3, bookRepository.findById("bk1").getCopiesAvailable());
     }
+
+    @Test
+    void noReserveNoCopiesAvailable() {
+        IBookRepository bookRepository = new MemoryBookRepository();
+        IReservationRepository reservationRepository = new MemoryReservationRepository();
+        ReservationService serviceReserve = new ReservationService(bookRepository, reservationRepository);
+        Book book = new Book("bk2", "Mowgli", 0);
+        bookRepository.save(book);
+        assertThrows(IllegalStateException.class, () -> {serviceReserve.reserve("u1", "bk2");});
+    }
 }

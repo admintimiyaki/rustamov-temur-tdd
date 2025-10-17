4de166993d4042986348af295ef4de44cedff7bd
admintimiyaki <timich.cs@icloud.com>
2025-10-17 16:48:05 +0100

    Add repositories

---
+++ src/main/java/IReservationRepository.java
@@ -0,0 +1,8 @@
+import java.util.*;
+interface IReservationRepository {
+    void save(Reservation reservation);
+    boolean existsByUserAndBook(String userId, String bookId);
+    List<Reservation> findByUser(String userId);
+    void delete(String userId, String bookId);
+}
+

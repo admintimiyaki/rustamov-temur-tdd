8487226cc0880f2fb8b10986cd778198ca195762
admintimiyaki <timich.cs@icloud.com>
2025-10-19 01:07:31 +0100

    Add priority class variable in User class

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,25 +1,39 @@
 public class User {
     private String id;
     private String name;
+    private boolean priority; // new field
 
-    public User(String id, String name){
+    public User(String id, String name) {
+        this(id, name, false);
+    }
+
+    public User(String id, String name, boolean priority) {
         this.id = id;
         this.name = name;
+        this.priority = priority;
     }
 
     public String getId() {
-        return this.id;
+        return id;
     }
 
-    public void setId(String id){
+    public void setId(String id) {
         this.id = id;
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public boolean isPriority() {
+        return priority;
+    }
+
+    public void setPriority(boolean priority) {
+        this.priority = priority;
     }
 }
